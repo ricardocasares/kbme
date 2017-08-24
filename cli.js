e596ded27779151a002e7e938068762f14d8f60b
Ricardo Casares <ricardocasares@gmail.com>
2017-08-24 19:08:04 +0200

    Add editorconfig, fix indentation

--- cli.js
+++ cli.js
@@ -5,21 +5,21 @@ const { validate, replace } = require('./util')
 const { request, metrics, csv} = require('./lib')
 
 const flags = mri(process.argv.slice(2), {
-    alias: {
-        c: 'csv',
-        k: 'keys',
-        t: 'types',
-        a: 'auto',
-        d: 'done',
-        t: 'todo',
-        u: 'user',
-        p: 'pass',
-        j: 'jira',
-        e: 'endpoint',
-        q: 'query',
-        h: 'help'
-    },
-    default: opt
+  alias: {
+    c: 'csv',
+    k: 'keys',
+    t: 'types',
+    a: 'auto',
+    d: 'done',
+    t: 'todo',
+    u: 'user',
+    p: 'pass',
+    j: 'jira',
+    e: 'endpoint',
+    q: 'query',
+    h: 'help'
+  },
+  default: opt
 });
 
 validate(flags)
@@ -27,14 +27,14 @@ validate(flags)
 main().then(console.log).catch(console.log);
 
 async function main() {
-    const api = request(flags);
-    const query = replace(flags.query, flags)
-    const issues = await api(query)
-    const res = metrics(flags, issues)
+  const api = request(flags);
+  const query = replace(flags.query, flags)
+  const issues = await api(query)
+  const res = metrics(flags, issues)
 
-    if (flags.csv) {
-        return csv(res)
-    }
+  if (flags.csv) {
+    return csv(res)
+  }
 
-    return res;
+  return res;
 }
--- util.js
+++ util.js
@@ -1,74 +1,74 @@
 module.exports = {
-    pad,
-    csv,
-    date_ago,
-    days_between,
-    num_reducer,
-    by_status,
-    replace,
-    validate,
+  pad,
+  csv,
+  date_ago,
+  days_between,
+  num_reducer,
+  by_status,
+  replace,
+  validate,
 }
 
 function pad(n) {
-    return ('0' + n).slice(-2)
+  return ('0' + n).slice(-2)
 }
 
 function csv(result) {
-    return Object.values(result).join(', ')
+  return Object.values(result).join(', ')
 }
 
 function date_ago(days) {
-    const now = new Date()
-    const last = new Date(now.getTime() - (days * 24 * 60 * 60 * 1000))
-    return `${last.getFullYear()}-${pad(last.getMonth() + 1)}-${pad(last.getDate())}`;
+  const now = new Date()
+  const last = new Date(now.getTime() - (days * 24 * 60 * 60 * 1000))
+  return `${last.getFullYear()}-${pad(last.getMonth() + 1)}-${pad(last.getDate())}`;
 }
 
 function days_between(a, b) {
-    const diff = Math.abs(a.getTime() - b.getTime())
-    return Math.ceil(diff / (1000 * 3600 * 24));
+  const diff = Math.abs(a.getTime() - b.getTime())
+  return Math.ceil(diff / (1000 * 3600 * 24));
 }
 
 function num_reducer(a, b) {
-    a += b
-    return a
+  a += b
+  return a
 }
 
 function by_status(status) {
-    return function({field, toString}) {
-        return (field === 'status' && toString === status)
-    }
+  return function({field, toString}) {
+    return (field === 'status' && toString === status)
+  }
 }
 
 function replace(target, options) {
-    for (let key in options) {
-        target = target.replace(`%${key}`, options[key]);
-    }
+  for (let key in options) {
+    target = target.replace(`%${key}`, options[key]);
+  }
 
-    return target;
+  return target;
 }
 
 function validate(flags) {
-    let error = false
+  let error = false
 
-    if (flags.help) {
-        console.log(require('./help'))
-        process.exit(0);
-    }
+  if (flags.help) {
+    console.log(require('./help'))
+    process.exit(0);
+  }
 
-    if (!flags.jira) {
-        error = true
-        console.log('Please configure your JIRA URL environment variable')
-    }
+  if (!flags.jira) {
+    error = true
+    console.log('Please configure your JIRA URL environment variable')
+  }
 
-    if (!flags.user) {
-        error = true
-        console.log('Please configure your JIRA user environment variable')
-    }
+  if (!flags.user) {
+    error = true
+    console.log('Please configure your JIRA user environment variable')
+  }
 
-    if (!flags.pass) {
-        error = true
-        console.log('Please configure your JIRA password environment variable')
-    }
+  if (!flags.pass) {
+    error = true
+    console.log('Please configure your JIRA password environment variable')
+  }
 
-    if (error) process.exit(1)
+  if (error) process.exit(1)
 }
